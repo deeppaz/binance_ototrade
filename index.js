2a955c8d568a4c1b99f88d4ae85987cf9eef6794
deeppaz <deeppaz@hotmail.com>
2019-05-27 16:58:04 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function tumsembolsAl(){
-  var istem1 = new XMLHttpRequest();
-  istem1.open('GET','https://api.binance.com/api/v3/ticker/price',true);
-  istem1.onload = function(){
+const $ = require('jquery');
+const $ = require('binance');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-  }
+var vr_sym = {};
+var vr_sympairs = {};
+var yeni_coin = '';
+var vr_krnmetre = 2*1000;
+var vr_karmarji = '';
+var vr_buyprecision;
+var vr_residueth
+
+var sat_coin = {
+  "eflag" : false,
+  "coin"  : "",
+  "coinp" : "",
+  "qty"   : "",
 }
+
+var vr_yuksel,vr_dus;
+var vr_coinmcap = {};
